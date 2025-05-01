becb4528be4c9cf53cb44697ffe9660300789030
Rob Keys <rob_keys@outlook.com>
2025-05-01 18:33:05 +0000

    Restructure

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors",1);
+spl_autoload_register(function ($classname){
+	include("/home/bitnami/bakehouse/private/$classname.php");
+});
+$controller = new Controller($_GET);
+$controller->run();
+?>
